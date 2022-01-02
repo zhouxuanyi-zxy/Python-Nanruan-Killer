9a029b366c424b97d0458f981fa873256eddcaf3
zhouxuanyi-zxy <69704410+zhouxuanyi-zxy@users.noreply.github.com>
2022-01-02 18:34:42 +0800

    Add files via upload

--- Python_Nanruan_Killer/Python_Nanruan_Killer.py
+++ Python_Nanruan_Killer/Python_Nanruan_Killer.py
@@ -13,7 +13,11 @@ import os.path
 def file():
     if os.path.isfile("pssuspend.exe"):
         if os.path.isfile("pskill.exe"):
-            print("文件检测完毕！")
+            if os.path.isfile("ntsd.exe"):
+                print("文件检测完毕！")
+            else:
+                print("缺少ntsd.exe!")
+                exit()
         else:
             print("缺少pskill.exe!")
             exit()
@@ -22,12 +26,13 @@ def file():
         exit()
 
 def pssuspend():
-    print("使用pssuspend挂起进程")
-    path = sys.path[0]
-    pssuspendfile = path + "\pssuspend.exe"
-    pssuspendfile = pssuspendfile + " StudentMain.exe"
-    print(pssuspendfile)
     os.system("chcp 65001")
+    print("使用pssuspend挂起进程")
+    pssuspendfilepath = sys.path[0]
+    #pssuspendfile = path + "\pssuspend.exe"
+    #pssuspendfile = pssuspendfile + " StudentMain.exe"
+    pssuspendfile = pssuspendfilepath + r"\pssuspend.exe StudentMain.exe"
+    print(pssuspendfile)
     os.system(pssuspendfile)
     
 def taskkill():
@@ -35,16 +40,27 @@ def taskkill():
     os.system("taskkill /F /T /IM StudentMain.exe")
 
 def pskill():
+    os.system("chcp 65001")
     print("使用pskill结束进程")
     pskill = sys.path[0]
-    pskill = pskill + "\pskill.exe"
-    pskill = pskill + " -t StudentMain.exe"
+    #pskill = pskill + "\pskill.exe"
+    #pskill = pskill + " -t StudentMain.exe"
+    pskill = pskill + r"\pskill.exe -t StudentMain.exe"
     print(pskill)
-    os.system("chcp 65001")
     os.system(pskill)
+
+def ntsd():
+    os.system("chcp 65001")
+    print("使用ntsd结束进程")
+    ntsd = sys.path[0]
+    ntsd = ntsd + r"\ntsd.exe -c q -pn StudentMain.exe"
+    print(ntsd)
+    os.system(ntsd)
+
 
 print("------ by zhouxuanyi_zxy ------")
 file()
 pssuspend()
 taskkill()
 pskill()
+ntsd()
